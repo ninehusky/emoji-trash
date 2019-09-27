6c89da027cde9f1bd103a44df82dbbe491b1f103
Andrew <acheung8@uw.edu>
2019-09-27 01:27:54 -0700

    idk what happened

--- server/data/table_editor.py
+++ server/data/table_editor.py
@@ -2,12 +2,16 @@ import sys, json
 
 def add_emojis():
     COMMON_WORDS = ["the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part"]
+    PRONOUNS = ['me', 'she', 'it', 'they', 'him', 'her', 'it', 'my', 'mine', 'your', 'yours', 'his', 'her', 'its', 'their', 'who', 'whom', 'whose', 'what', 'which', 'another', 'each', 'everything', 'nobody', 'either', 'someone', 'this', 'myself', 'yourself', 'himself', 'herself', 'itself', 'themselves']
+    OTHER_WORDS = ['everything', 'something', 'nothing', 'anything', 'any', 'none']
+    PROFANITY = []
+
     data = None
     with open('tempdata.json', 'r') as write_file:
         data = json.load(write_file)        
     
     with open('tempdata.json', 'w+') as write_file:
-        for word in COMMON_WORDS:
+        for word in PRONOUNS:
             emoji = input('Give me an emoji that represents {} (Enter to skip): '.format(word))
             if emoji:
                 data[word] = emoji
